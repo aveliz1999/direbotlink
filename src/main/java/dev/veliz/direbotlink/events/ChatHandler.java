b407f1ab9d175e4d640b78827ca94039d815ad4b
aveliz1999 <github@veliz99.com>
2021-05-31 12:55:41 -0400

    Update ChatHandler to match server specifications

--- src/main/java/dev/veliz/direbotlink/events/ChatHandler.java
+++ src/main/java/dev/veliz/direbotlink/events/ChatHandler.java
@@ -14,18 +14,22 @@ public class ChatHandler {
         AsyncHttpClient client = Dsl.asyncHttpClient();
 
         JsonObject obj = new JsonObject();
-        obj.addProperty("player", event.getPlayer().getDisplayName().getString());
-        obj.addProperty("id", event.getPlayer().getStringUUID());
-        obj.addProperty("text", event.getMessage());
+        obj.addProperty("serverId", Config.serverId);
+        obj.addProperty("apiKey", Config.apiKey);
+        obj.addProperty("minecraftUsername", event.getPlayer().getDisplayName().getString());
+        obj.addProperty("minecraftUuid", event.getPlayer().getStringUUID());
+        obj.addProperty("message", event.getMessage());
 
         Request request = Dsl
                 .post(Config.chatWebhook)
                 .setBody(obj.toString())
                 .setHeader("Authorization", "Bearer " + Config.apiKey)
+                .setHeader("Content-Type", "application/json")
                 .build();
         client.executeRequest(request, new AsyncCompletionHandler<Object>() {
             @Override
             public Object onCompleted(Response response) throws Exception {
+                System.out.println(response.getResponseBody());
                 return response;
             }
         });
